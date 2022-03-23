70964c7cfd4ab08bbaa5979f3d44ad8c635ec450
BohdanYeremenko <64425309+BohdanYeremenko@users.noreply.github.com>
2022-03-23 15:55:24 +0100

    Update detectron2_custom_coco_data_segmentation.py
    Changes of the tests dataset

--- devel/detectron/detectron2_quickstart/detectron2_custom_coco_data_segmentation.py
+++ devel/detectron/detectron2_quickstart/detectron2_custom_coco_data_segmentation.py
@@ -38,7 +38,7 @@ print("Png= ",  pathToPng)
 register_coco_instances("Parenhyma", {},pathToJson, pathToPng) 
 
 TestJpg=str(input_data_dir / "png-testing/Tx030D_Ven-20220314T115944Z-001/Tx030D_Ven")
-register_coco_instances("Parenhyma_Test", {}, TestJpg)
+register_coco_instances("Parenhyma_Test", {}, pathToJson, pathToPng)
 fruits_nuts_metadata = MetadataCatalog.get("Parenhyma")
 dataset_dicts = DatasetCatalog.get("Parenhyma")
 
