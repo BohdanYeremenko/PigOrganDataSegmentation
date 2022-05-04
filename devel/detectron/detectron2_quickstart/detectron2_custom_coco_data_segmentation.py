862b22e2cee221d509a05a91763f98d1591c08fd
BohdanYeremenko <64425309+BohdanYeremenko@users.noreply.github.com>
2022-05-04 22:26:06 +0200

    Try to play with dataset

--- devel/detectron/detectron2_quickstart/detectron2_custom_coco_data_segmentation.py
+++ devel/detectron/detectron2_quickstart/detectron2_custom_coco_data_segmentation.py
@@ -33,6 +33,8 @@ logger.debug(str(Path(input_data_dir).glob("**/*")))
 from detectron2.data.datasets import register_coco_instances
 pathToJsonTrain=str(input_data_dir / "coco_training/TrainingCoco1/Traning_Coco.json")
 pathToJsonTest=str(input_data_dir / "coco_testing/coco_testing1/Testing_Coco3.json")
+pathToJsonFinale=str(input_data_dir / "coco_training/26/annotations/instances_default.json")
+pathToPngFinale=(input_data_dir / "png-training/Tx026/Tx026D_Ven")
 pathToPng=str(input_data_dir / "png_full/PNG2")
 print("Json= ",  pathToJsonTrain)
 print("Json2= ", pathToJsonTest)
@@ -41,6 +43,7 @@ register_coco_instances("Parenhyma", {},pathToJsonTrain, pathToPng)
 
 TestJpg=str(input_data_dir / "png-testing/Tx030D_Ven-20220314T115944Z-001/Tx030D_Ven")
 register_coco_instances("Parenhyma_Test", {}, pathToJsonTest, pathToPng)
+register_coco_instances("Parenhyma_Final", {}, pathToJsonFinale, pathToPngFinale)
 fruits_nuts_metadata = MetadataCatalog.get("Parenhyma")
 dataset_dicts = DatasetCatalog.get("Parenhyma")
 
@@ -67,28 +70,28 @@ import os
 cfg = get_cfg()
 cfg.merge_from_file(f"/auto/vestec1-elixir/home/bohdany/detectron2/configs/COCO-InstanceSegmentation/mask_rcnn_R_50_FPN_3x.yaml")
 cfg.DATASETS.TRAIN = ("Parenhyma",)
-cfg.DATASETS.TEST = ()   # no metrics implemented for this dataset
+cfg.DATASETS.TEST = ("Parenhyma_Test",)   # no metrics implemented for this dataset
 cfg.DATALOADER.NUM_WORKERS = 2
 cfg.MODEL.WEIGHTS = "detectron2://COCO-InstanceSegmentation/mask_rcnn_R_50_FPN_3x/137849600/model_final_f10217.pkl"  # initialize from model zoo
 cfg.SOLVER.IMS_PER_BATCH = 1 #kolik obrazku v 1 okamžik na grafickou kartou
 cfg.SOLVER.BASE_LR = 0.03 # jak intenzivně měnime Váhy při backPropagation.
-cfg.SOLVER.MAX_ITER = 1000    # 300 iterations seems good enough, but you can certainly train longer
+cfg.SOLVER.MAX_ITER = 300    # 300 iterations seems good enough, but you can certainly train longer
 cfg.MODEL.ROI_HEADS.BATCH_SIZE_PER_IMAGE = 20   # faster, and good enough for this toy dataset
-cfg.MODEL.ROI_HEADS.NUM_CLASSES = 9 # 4 classes (data, fig, hazelnut)
+cfg.MODEL.ROI_HEADS.NUM_CLASSES = 3 # 4 classes (data, fig, hazelnut)
 
 os.makedirs(cfg.OUTPUT_DIR, exist_ok=True)
-print("trainer_started")
+print(" trainer_started ")
 trainer = DefaultTrainer(cfg)
 trainer.resume_or_load(resume=False)
 trainer.train()
-print("trainer_finished")
+print(" trainer_finished ")
 
 cfg.MODEL.WEIGHTS = os.path.join(cfg.OUTPUT_DIR, "model_final.pth")
 cfg.MODEL.ROI_HEADS.SCORE_THRESH_TEST = 0.5   # set the testing threshold for this model
 cfg.DATASETS.TEST = ("Parenhyma_Test", )
-print("test_started")
+print(" test_started ")
 predictor = DefaultPredictor(cfg)
-print("test_finished")
+print(" test_finished ")
 
 from detectron2.utils.visualizer import ColorMode
 
