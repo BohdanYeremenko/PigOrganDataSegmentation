3ee16ca981f6fc520f1ee62a8f4a4edbe1eee9cb
BohdanYeremenko <64425309+BohdanYeremenko@users.noreply.github.com>
2022-08-14 16:47:19 +0200

    Update detectron2_custom_coco_data_segmentation.py
    Print for help

--- devel/detectron/detectron2_quickstart/detectron2_custom_coco_data_segmentation.py
+++ devel/detectron/detectron2_quickstart/detectron2_custom_coco_data_segmentation.py
@@ -66,7 +66,6 @@ for d in dataset_dicts:
 from detectron2.engine import DefaultTrainer
 from detectron2.config import get_cfg
 import os
-
 cfg = get_cfg()
 cfg.merge_from_file(f"/auto/vestec1-elixir/home/bohdany/detectron2/configs/COCO-InstanceSegmentation/mask_rcnn_R_50_FPN_3x.yaml")
 print("Parenhyma")
@@ -76,13 +75,21 @@ print("Parenhyma_Test")
 cfg.DATASETS.TEST = ("Parenhyma_Test",)   # no metrics implemented for this dataset
 print("Parenhyma_Test-ok")
 cfg.DATALOADER.NUM_WORKERS = 2
+print("NUM_WORKERS ok")
 cfg.MODEL.WEIGHTS = "detectron2://COCO-InstanceSegmentation/mask_rcnn_R_50_FPN_3x/137849600/model_final_f10217.pkl"  # initialize from model zoo
+print("model weights ok")
 cfg.SOLVER.IMS_PER_BATCH = 1 #kolik obrazku v 1 okamžik na grafickou kartou
+print("kolik obrazu ok")
 cfg.SOLVER.BASE_LR = 0.03 # jak intenzivně měnime Váhy při backPropagation.
+print("intenyita ok")
 cfg.SOLVER.MAX_ITER = 300    # 300 iterations seems good enough, but you can certainly train longer
+print("iter ok")
 cfg.MODEL.ROI_HEADS.BATCH_SIZE_PER_IMAGE = 20   # faster, and good enough for this toy dataset
+print("mODEL.ROI_HEADS.BATCH_SIZE_PER_IMAGE ok")
 cfg.MODEL.ROI_HEADS.NUM_CLASSES = 3 # 4 classes (data, fig, hazelnut)
+print("classes ok")
 cfg.OUTPUT_DIR=outputdir
+print("outputdir ok")
 os.makedirs(cfg.OUTPUT_DIR, exist_ok=True)
 print(" trainer_started ")
 trainer = DefaultTrainer(cfg)
@@ -113,4 +120,4 @@ for d in dataset_dicts2:
     file_path.parent.mkdir(parents=True, exist_ok=True)
     cv2.imwrite(str(file_path), v.get_image()[:, :, ::-1])
     # cv2_imshow(v.get_image()[:, :, ::-1])
-
+print("all ok")
