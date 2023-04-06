6c3962138c0db2296ce4ea4f2d541983aaf6e2b0
BohdanYeremenko <64425309+BohdanYeremenko@users.noreply.github.com>
2023-04-06 02:42:00 +0200

    Images3in1  0.0001

--- devel/detectron/detectron2_quickstart/detectron2_custom_coco_data_segmentation.py
+++ devel/detectron/detectron2_quickstart/detectron2_custom_coco_data_segmentation.py
@@ -36,7 +36,7 @@ pathToJsonTrain=str(input_data_dir / "Coco_Dataset/Trenovani.json")
 pathToJsonTest=str(input_data_dir / "Coco_Dataset/Validace.json")
 pathToJsonFinale=str(input_data_dir / "Coco_Dataset/Testovani.json")
 #pathToPngFinale=(input_data_dir / "png-training/Tx026/Tx026D_Ven")
-pathToPng=str(input_data_dir / "png_full/images")
+pathToPng=str(input_data_dir / "png_full/Images3in1")
 print("Json= ",  pathToJsonTrain)
 print("Json2= ", pathToJsonTest)
 print("Png= ",  pathToPng)
@@ -82,7 +82,7 @@ cfg.MODEL.WEIGHTS = "detectron2://COCO-InstanceSegmentation/mask_rcnn_R_50_FPN_3
 print("model weights ok")
 cfg.SOLVER.IMS_PER_BATCH = 10 #kolik obrazku v 1 okamžik na grafickou kartou
 print("kolik obrazu ok")
-cfg.SOLVER.BASE_LR = 0.00001 # jak intenzivně měnime Váhy při backPropagation.
+cfg.SOLVER.BASE_LR = 0.0001 # jak intenzivně měnime Váhy při backPropagation.
 print("intenyita ok")
 cfg.SOLVER.MAX_ITER = 12000    # 300 iterations seems good enough, but you can certainly train longer
 print("iter ok")
