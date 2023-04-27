f96b344c725b7f11e39676bd47e0136812a9200a
BohdanYeremenko <64425309+BohdanYeremenko@users.noreply.github.com>
2023-04-27 21:15:00 +0200

    40000Iter

--- devel/detectron/detectron2_quickstart/detectron2_custom_coco_data_segmentation.py
+++ devel/detectron/detectron2_quickstart/detectron2_custom_coco_data_segmentation.py
@@ -84,7 +84,7 @@ cfg.SOLVER.IMS_PER_BATCH = 10 #kolik obrazku v 1 okamžik na grafickou kartou
 print("kolik obrazu ok")
 cfg.SOLVER.BASE_LR = 0.001 # jak intenzivně měnime Váhy při backPropagation.
 print("intenyita ok")
-cfg.SOLVER.MAX_ITER = 30000   # 300 iterations seems good enough, but you can certainly train longer
+cfg.SOLVER.MAX_ITER = 50000   # 300 iterations seems good enough, but you can certainly train longer
 print("iter ok")
 cfg.MODEL.ROI_HEADS.BATCH_SIZE_PER_IMAGE = 5   # faster, and good enough for this toy dataset
 print("mODEL.ROI_HEADS.BATCH_SIZE_PER_IMAGE ok")
